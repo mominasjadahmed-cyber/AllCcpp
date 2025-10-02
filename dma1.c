f3d2275f9f1c3f4ae814c7c17ac254a5b0c7a932
Asjad <mominasjadahmed@gmail.com>
2025-10-02 16:28:31 +0530

    The dma1 file is updated

--- dma1.c
+++ dma1.c
@@ -1,8 +1,25 @@
-// this program prints Hello World to the console
 #include<stdio.h>
-int main ()
-{
-    printf("Hello World\n");
+#include<stdlib.h>
+
+int main() {
+    int n,i,*ptr;
+    printf("\n Enter number of elements to allocate: ");
+    scanf("%d", &n);
+    ptr = (int*)malloc(n * sizeof(int));
+    printf("\n Meomery is allocated =%u", ptr);
+    for(i=0; i<n; i++) {
+        *(ptr + i) = i * 100;
+    }
+    printf("\n Fifth Location = %u", *(ptr + 0));
+    printf("\n Second Location = %u", *(ptr + 1));
+
+    free (ptr);
+    printf("\nMeomery is deallocated");
+
+    for(i=0; i<n; i++) {
+        printf("\n %d", *(ptr + i));
+    }
+
+
     return 0;
-    
 }
